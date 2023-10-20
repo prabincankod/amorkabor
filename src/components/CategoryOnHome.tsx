4cce21b65b884b2f0f86a8388bde52f89763578d
Prabin <42871240+prabincankod@users.noreply.github.com>
2023-10-20 22:38:00 +0545

    conditional rendering when category has no articles

--- src/components/CategoryOnHome.tsx
+++ src/components/CategoryOnHome.tsx
@@ -15,30 +15,33 @@ const CategoryOnHome = () => {
   return (
     <>
       {categoryWithNews &&
-        categoryWithNews.map((categoryAndNews) => (
-          <div className="mx-10">
-            <div className="flex flex-row justify-between">
-              <div className="lg:4xl mt-2 text-xl font-extrabold text-primary md:text-3xl">
-                {categoryAndNews.title}
-              </div>
-              <div className="lg:4xl mt-2 text-xl  text-primary md:text-3xl ">
-                {/* <ChevronRightIcon className="rounded-full bg-primary-foreground p-1" /> */}
-                {categoryAndNews.slug}
-              </div>
-            </div>
-
-            <div className=" mt-2   flex flex-row flex-wrap place-content-center justify-center gap-x-6 gap-y-2 ">
-              {categoryAndNews.articles.map((news) => (
-                <div className="card rounded-md border bg-accent ">
-                  <div className="image  w-52 lg:w-64">
-                    <img src={news.image} className=" rounded-md " alt="" />
+        categoryWithNews.map(
+          (categoryAndNews) =>
+            // Check if there are articles in the category
+            categoryAndNews.articles.length > 0 && (
+              <div className="mx-10">
+                <div className="flex flex-row justify-between">
+                  <div className="lg:4xl mt-2 text-xl font-extrabold text-primary md:text-3xl">
+                    {categoryAndNews.title}
                   </div>
-                  <div className="title  w-52  text-center">{news.title}</div>
+                  <div className="lg:4xl mt-2 text-xl text-primary md:text-3xl ">
+                    {categoryAndNews.slug}
+                  </div>
                 </div>
-              ))}
-            </div>
-          </div>
-        ))}
+
+                <div className="mt-2 flex flex-row flex-wrap place-content-center justify-center gap-x-6 gap-y-2">
+                  {categoryAndNews.articles.map((news) => (
+                    <div className="card items-center rounded-md border bg-accent">
+                      <div className="image w-52 lg:w-64">
+                        <img src={news.image} className="rounded-md" alt="" />
+                      </div>
+                      <div className="title w-52 text-center">{news.title}</div>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            ),
+        )}
     </>
   );
 };
